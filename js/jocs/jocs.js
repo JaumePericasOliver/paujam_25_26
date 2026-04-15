ac505c119d91891b0fe2b359becf0d5c0ace66bb
Marc González <marcgonzalez1@paucasesnovescifp.cat>
2026-04-15 10:30:52 +0200

    jocs.js acabat

---
+++ js/jocs/jocs.js
@@ -0,0 +1,82 @@
+const jocs = [
+    {
+        video: "../images/video.webp",
+        imatge: "../images/image.webp",
+        titol: "Title 1",
+        descripcio: "Texto descriptivo 1",
+        autor: "Author 1",
+        perfil: "../images/profile.webp"
+    },
+    {
+        video: "../images/video.webp",
+        imatge: "../images/image.webp",
+        titol: "Title 2",
+        descripcio: "Texto descriptivo 2",
+        autor: "Author 2",
+        perfil: "../images/profile.webp"
+    },
+    {
+        video: "../images/video.webp",
+        imatge: "../images/image.webp",
+        titol: "Title 3",
+        descripcio: "Texto descriptivo 3",
+        autor: "Author 3",
+        perfil: "../images/profile.webp"
+    },
+    {
+        video: "../images/video.webp",
+        imatge: "../images/image.webp",
+        titol: "Title 4",
+        descripcio: "Texto descriptivo 4",
+        autor: "Author 4",
+        perfil: "../images/profile.webp"
+    },
+    {
+        video: "../images/video.webp",
+        imatge: "../images/image.webp",
+        titol: "Title 5",
+        descripcio: "Texto descriptivo 5",
+        autor: "Author 5",
+        perfil: "../images/profile.webp"
+    },
+    {
+        video: "../images/video.webp",
+        imatge: "../images/image.webp",
+        titol: "Title 6",
+        descripcio: "Texto descriptivo 6",
+        autor: "Author 6",
+        perfil: "../images/profile.webp"
+    },
+];
+
+let index = 0;
+
+function actualitzar(){
+    document.getElementById("video").src = jocs[index].video;
+    document.getElementById("imatge").src = jocs[index].imatge;
+    document.getElementById("titol").textContent = jocs[index].titol;
+    document.getElementById("descripcio").textContent = jocs[index].descripcio;
+    document.getElementById("autor").textContent = jocs[index].autor;
+    document.getElementById("perfil").src = jocs[index].perfil;
+
+    document.getElementById("comptador").textContent =
+        (index + 1) + " de " + jocs.length;
+}
+
+function seguent(){
+    index++;
+    if(index >= jocs.length){
+        index = 0;
+    }
+    actualitzar();
+}
+
+function anterior(){
+    index--;
+    if(index < 0){
+        index = jocs.length - 1;
+    }
+    actualitzar();
+}
+
+window.addEventListener("DOMContentLoaded", actualitzar);
